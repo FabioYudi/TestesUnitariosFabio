52b1dfa861bb4322f9efbdc2a3a1b70c55005a9c
fabioyudi <fabioyudi123456>
2018-02-15 10:42:51 -0200

    novos testes

--- src/test/java/com/github/fabioyudi/Servicos/LocacaoServiceTest.java
+++ src/test/java/com/github/fabioyudi/Servicos/LocacaoServiceTest.java
@@ -19,7 +19,7 @@ public class LocacaoServiceTest {
         LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Usuario 1");
         Filme filme = new Filme("Filme 1", 2, 5.0);
-        //acaoa
+        //acao
         Locacao locacao = service.alugarFilme(usuario, filme);
 
         //verificacao
